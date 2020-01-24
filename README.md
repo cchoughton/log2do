0564854ad9eb59002efd7ebd08252498fe117dca
cchoughton <charliehoughton7@gmail.com>
2020-01-24 12:15:07 -0500

    Add install script, readme, updates

---
+++ README.md
@@ -0,0 +1,6 @@
+# Simple Stata .log to .do file converter.
+
+TODO:
+
+Add support for logs that require multiple lines
+Add support for formatted log files
---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [[ $# -eq 0 ]]; then
+    install_dir="/usr/local/bin"
+    echo "No install directory given, defaulting to $install_dir"
+else
+    install_dir=$1
+fi
+
+sudo cp log2do $install_dir
+
+if [[ $? -ne 0 ]]; then
+    err=$?
+    echo "Error: $err"
+    echo "Could not install log2do"
+    exit $err
+fi
+
+echo "Installed to $install_dir" 
